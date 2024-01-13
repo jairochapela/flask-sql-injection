867ed09a30b1b048cb46986079ec79f38df528ef
Jairo Chapela-Martinez <jairochapela@gmail.com>
2024-01-13 10:40:52 +0100

    Retoques estéticos.

--- createdb.py
+++ createdb.py
@@ -1,11 +1,14 @@
 # Script para crear y poblar la base de datos SQLite:
-
 import sqlite3
 
-with sqlite3.connect('database.db') as conn:
-    cur = conn.cursor()
+def crear_db():
+    with sqlite3.connect('database.db') as conn:
+        cur = conn.cursor()
 
-    cur.execute('CREATE TABLE IF NOT EXISTS users (username VARCHAR, password VARCHAR)')
-    cur.execute('INSERT INTO users VALUES (?,?)', ('admin', 'admin'))
-    cur.execute('INSERT INTO users VALUES (?,?)', ('user', 'user'))
+        cur.execute('CREATE TABLE IF NOT EXISTS users (username VARCHAR, password VARCHAR)')
+        cur.execute('INSERT INTO users VALUES (?,?)', ('admin', 'admin'))
+        cur.execute('INSERT INTO users VALUES (?,?)', ('user', 'user'))
 
+
+if __name__ == '__main__':
+    crear_db()
